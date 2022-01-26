2fa155416e373b8020024a01ece6026378ce693b
Pedro Gomes <pedro.gomes@somosquiq.com>
2022-01-25 23:58:46 -0300

    git commit -m "first commit"

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Container, Text } from '@chakra-ui/react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Container>
+            <Box>
+                <Text>Text</Text>
+            </Box>
+        </Container>
+    )
 }
 
-export default App;
+export default App
